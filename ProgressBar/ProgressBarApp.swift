1a31910e2aad12272eac78630cbd50f10f3e04d5
githubuiux <uiuxista@gmail.com>
2023-09-13 16:40:02 +0300

    Initial Commit

---
+++ ProgressBar/ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarApp.swift
+//  ProgressBar
+//
+//  Created by Levent on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
